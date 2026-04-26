c5ca233f9715e2d742ccf6c063b8e800096a465e
mikhail <mixa64517@gmail.com>
2026-04-26 16:22:53 +0300

    ttggg

--- main.py
+++ main.py
@@ -1,47 +1,17 @@
-from tkinter import ttk
-from tkinter import *
-from tkinter.messagebox import showinfo,showwarning
-root=Tk()
-root.geometry("700x450")
-root.title("калькулятор")
+#Функциональные требования:
+#1.простейшие функции и цифры (+,-,*,/)
+#2.наличие доп. функций(корень,возведение в степень,факториал,проценты)
+#3.наличие тригонометричиских функций
+#4.наличие конвертера(температура,длина,вес)
+#5.использование try exsept (деление на 0,буквы и тд.)
+#6.наличие поля ввода
+#7.использование button
+#8.построение кода на функциях
+#9.использование виджета menu
 
-znak=["+","-","*","/"]
-
-
-label=ttk.Label(text="первое число")
-label.pack(anchor="nw")
-
-entr=ttk.Entry()
-entr.pack(anchor="nw")
-
-label2=ttk.Label(root,text="второе число")
-label2.pack(anchor="nw")
-
-entr2=ttk.Entry(root)
-entr2.pack(anchor="nw")
-
-firstnumber=entr.get()
-secondnumber=entr2.get()
-
-def counter():
-    window=Tk()
-
-    global a
-
-    combo=ttk.Combobox(window,values=znak)
-    combo.pack(anchor="nw")
-    a=combo.get()
-
-btn=ttk.Button(text="знак",command=counter)
-btn.pack(anchor="nw",pady=10)
-
-def schet():
-    global result
-    if a == "+":
-        result=firstnumber+secondnumber
-        showinfo("результат",message=result)
-
-btn4=ttk.Button(text="=",command=schet)
-btn4.pack(anchor="nw",pady=10)
-
-root.mainloop()
+#графические требования:
+#1.белый фон
+#2.основной цвет серый
+#3.акцентный цвет синий
+#4.дополнительный цвет красный
+#5.весь текст черного цвета
